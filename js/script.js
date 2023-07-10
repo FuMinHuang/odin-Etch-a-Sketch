0cf86cc0ae6bbfbbda6e33c6b07cc0eb3d9be5e9
ILoveEgg0001 <98178807+ILoveEgg0001@users.noreply.github.com>
2023-07-10 14:32:08 -0700

    Add Hover effect in script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ for (i = 0; i < 256; i++) {
     const square = document.createElement("div");
     square.className = "square";
     document.querySelector(".container").appendChild(square);
-}
 
+    // Hover effect     
+    square.addEventListener(
+        "mouseenter",
+        (event) => {
+            // highlight the mouseenter target
+            event.target.style.backgroundColor = "aqua";
+
+            // reset the color after a short delay
+            setTimeout(() => {
+                event.target.style.backgroundColor = "";            
+            }, 500);
+        },
+        false,
+    );    
+}
