3f0a242fda22aefa87ccb5466de9a8c741416487
ILoveEgg0001 <98178807+ILoveEgg0001@users.noreply.github.com>
2023-07-11 15:11:45 -0700

    Completed the assignment
    - Add create grid function
    - Add hover effect
    - Add button for user input square number per side

--- js/script.js
+++ js/script.js
@@ -1,21 +1,45 @@
-// Create a 16x16 grid of square divs
-for (i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    document.querySelector(".container").appendChild(square);
+const container = document.querySelector("#container");
 
-    // Hover effect     
-    square.addEventListener(
-        "mouseenter",
-        (event) => {
-            // highlight the mouseenter target
-            event.target.style.backgroundColor = "aqua";
+// Create a grid of square divs
+function createGrid(numSquare) {
+    for (let i = 0; i < numSquare; i++) {
+        const row = container.appendChild(document.createElement("div"));
+        row.className = "row";
+        for (let j = 0; j < numSquare; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            row.appendChild(square);
 
-            // reset the color after a short delay
-            setTimeout(() => {
-                event.target.style.backgroundColor = "";            
-            }, 500);
-        },
-        false,
-    );    
+            // Hover effect 
+            square.addEventListener(
+                "mouseenter",
+                (event) => {
+                    // highlight the mouseenter target
+                    event.target.style.backgroundColor = "aqua";
+
+                    //reset the color after a short delay
+                    setTimeout(() => {
+                        event.target.style.backgroundColor = "";                        
+                    }, 500);
+                }
+            )
+        }       
+    }    
 }
+
+// Create a basic 16 x 16 grid
+createGrid(16);
+
+// Asking squares per side input
+let squaresInput = 0;
+const gridsButton = document.querySelector(".grids");
+gridsButton.addEventListener("click", () => {
+    // Remove the existing grid
+    const container = document.querySelector("#container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Asking for the number of squares per side for the new grid
+    squaresInput = prompt("How many squares per side for the new grid?", );
+    createGrid(squaresInput);});
